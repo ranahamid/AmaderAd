1479953871e157d76350aa8feadf40913bef2c35
RANA HAMID <1015052066.mrh@grad.cse.buet.ac.bd>
2018-04-30 01:06:24 +0600

    Hasan bhai...import

---
+++ AmaderAd/Controllers/ManabzaminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AmaderAd.Models;
+
+namespace AmaderAd.Controllers
+{
+    public class ManabzaminController : BaseController
+    {
+        // GET: Manabzamin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //classified advertisement
+        public ActionResult ClassifiedAdvertisement()
+        {
+            Newspaper model = new Newspaper
+            {
+                NewspaperName = "Manab Zamin",
+                AdLocation = "Manab Zamin Classified Advertisement",
+                AllAdCategory = GetAllAdCategory()
+            };
+            return View(model);
+        }
+
+    }
+}
---
+++ AmaderAd/Controllers/NewNationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AmaderAd.Models;
+
+namespace AmaderAd.Controllers
+{
+    public class NewNationController : BaseController
+    {
+        // GET: NewNation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //classified advertisement
+        public ActionResult ClassifiedAdvertisement()
+        {
+            Newspaper model = new Newspaper
+            {
+                NewspaperName = "New Nation",
+                AdLocation = "New Nation Classified Advertisement",
+                AllAdCategory = GetAllAdCategory()
+            };
+            return View(model);
+        }
+    }
+}
