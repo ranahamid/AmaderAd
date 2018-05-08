83a3677386f42da40cd50919a4bc0d1e9d196e23
RANA HAMID <1015052066.mrh@grad.cse.buet.ac.bd>
2018-05-09 00:03:05 +0600

    trim

--- AmaderAd/Controllers/NewAgeController.cs
+++ AmaderAd/Controllers/NewAgeController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AmaderAd.Models;
 
 namespace AmaderAd.Controllers
 {
-    public class NewAgeController : Controller
+    public class NewAgeController : BaseController
     {
         // GET: NewAge
         public ActionResult Index()
