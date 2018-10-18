d1842738fa67f1af3f9804e6fba4808f6f79474e
Md. Rana Hamid <1015052066.mrh@grad.cse.buet.ac.bd>
2018-10-19 02:09:34 +0600

    ssd

--- AmaderAd/App_Start/BundleConfig.cs
+++ AmaderAd/App_Start/BundleConfig.cs
@@ -13,6 +13,10 @@ namespace AmaderAd
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
              "~/Content/jquery/jquery-ui-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            )
+            );
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
